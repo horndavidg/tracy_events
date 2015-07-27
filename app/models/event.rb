05e65eb1c089af7f84927214dd82b787cb7e3f9a
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-07-27 10:09:40 -0700

    models work

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ActiveRecord::Base
 
-	validates: :description, :long, :lat, :time, :address, :duration, :date, :creator_id, presence: true
+	validates :description, :long, :lat, :time, :address, :duration, :date, :creator_id, presence: true
 	has_many :event_users, dependent: :destroy
 	has_many :users, through: :event_users
-	has many :photos, dependent: :destroy
+
+	has_many :photos, dependent: :destroy
 	has_many :comments, dependent: :destroy
 end
