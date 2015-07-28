f08ef3edeaa22e368a2c0879edd8efcdaf75139f
David Horn <horndavidg@gmail.com>
2015-07-27 18:10:24 -0700

    no CRUD on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,71 @@
 class CommentsController < ApplicationController
+  
+
+
+# -----------------------------
+
   def edit
   end
 
+# ------------------------------
+
   def new
   end
+
+# ------------------------------
+
+def create
+
+  @comment = Comment.new comment_params
+
+if @current_user
+   @comment.creator_id = @current_user.id
+  @comment.creator_name = @current_user.name
+# @comment.creator_id = @current_user.google_id
+end  
+
+@comment.event_id = params[:event_id]
+
+if @comment.save
+	redirect_to event_path(@comment.event_id), flash: {success: "Comment Added!"}
+    else
+    render "events/show"
+end	
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private  
+
+def comment_params
+    params.require(:comment).permit(
+      :comment
+    )
+end
+
+
+
+
+
 end
