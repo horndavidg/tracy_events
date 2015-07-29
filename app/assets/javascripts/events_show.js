a5f10b20c78e28c04d8af4defc5ac469f9b87529
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-07-29 13:09:16 -0700

    both dropdowns work. starting on single page add

--- app/assets/javascripts/events_show.js
+++ app/assets/javascripts/events_show.js
@@ -1,6 +1,28 @@
 $(".events.show").ready(function() {
+
+	//Adding a photo
 	$("#add_photos_button").click(function(e) {
 		e.preventDefault()
-		$( "#photo_drop_down" ).slideToggle( "slow" )
+		if($("#helper_div").attr("display") !== "none") {
+			$( "#photo_drop_down" ).slideToggle( "fast" )
+			$("#helper_div").toggleClass("slide")
+			// $("#helper_div").attr("display", "none")
+		}
+		
+
 	})
+
+
+	//Adding a comment
+	$("#add_comments_button").click(function(e) {
+		e.preventDefault()
+		// if($("#add_photo_button").attr("display") === "none" && $("#helper_div").attr("display") !== "none") {
+			console.log("HELLO")
+			$( "#comment_drop_down" ).slideToggle( "fast" )	
+		// }
+		
+		
+	})
+
+
 })
